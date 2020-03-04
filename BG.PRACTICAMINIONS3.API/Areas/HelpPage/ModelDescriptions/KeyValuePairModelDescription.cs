a52d2910dbbe6a999d78c05faa540a8213502aa7
maikolchf <maikolchf@outlook.com>
2020-03-04 08:45:14 -0600

    primer commit
    carga de practica de angular

---
+++ BG.PRACTICAMINIONS3.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BG.PRACTICAMINIONS3.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ BG.PRACTICAMINIONS3.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BG.PRACTICAMINIONS3.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
